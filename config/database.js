50bfe2160498550b97f605a2771fe879a04367be
Glaucio Daniel <glauciodaniel@gmail.com>
2020-07-20 21:46:15 -0300

    Server Production

--- config/database.js
+++ config/database.js
@@ -77,7 +77,7 @@ module.exports = {
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      database: Env.get('DB_DATABASE', 'dd35vrqpgm0l0u')
     },
     debug: Env.get('DB_DEBUG', false)
   }
